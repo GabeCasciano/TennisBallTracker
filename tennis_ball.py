4e85087d58dab3075d1a073c214d269e1e97a147
unknown <gabecasciano@gmail.com>
2020-04-15 13:20:48 -0700

    fix2, maybe? 2

--- tennis_ball.py
+++ tennis_ball.py
@@ -69,7 +69,7 @@ def main_loop():
                 cv2.circle(frame, center, 5, (0,0,255), -1)
 
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(0) & 0xFF
+        key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
             print("Quit")
