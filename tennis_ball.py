b529413a15ead955ad74c3c2355a1b98f6485eec
unknown <gabecasciano@gmail.com>
2020-04-15 14:23:23 -0700

    fix5, colors

--- tennis_ball.py
+++ tennis_ball.py
@@ -51,8 +51,9 @@ def main_loop():
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert to HSV color space
 
         mask = cv2.inRange(hsv, lower, upper) # Selecting color from image based on bounds
-        mask = cv2.erode(mask, None, iterations=2) # Erode the image
-        mask = cv2.dilate(mask, None, iterations=2) #
+        mask = cv2.bitwise_and(hsv, hsv, mask=mask)
+        #mask = cv2.erode(mask, None, iterations=2) # Erode the image
+        #mask = cv2.dilate(mask, None, iterations=2) #
 
         # Find the contours on the mask and find the current center of the ball
         contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
